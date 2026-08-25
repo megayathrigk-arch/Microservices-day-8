daba55f0de36b6431bda24a216ae5232f871fce4
megayathrigk-arch <me.gayathrigk@gmail.com>
2026-08-25 11:02:50 +0530

    Add files via upload

---
+++ Config_Server/src/main/java/com/example/demo/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+		
+		System.out.println("Application started");
+	}
+
+}
